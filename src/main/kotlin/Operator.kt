962f786cff721312ee47a553953f75667a2391fd
Nikunjbansal99 <500069944@stu.upes.ac.in>
2020-06-07 20:58:48 +0530

    File Modified

--- src/main/kotlin/Operator.kt
+++ src/main/kotlin/Operator.kt
@@ -2,14 +2,10 @@ sealed class Operator(val x: Long) {
     abstract fun calculate(y: Long): Long
 
     class add(x: Long) : Operator(x) {
-        override fun calculate(y: Long): Long {
-            return x + y
-        }
+        override fun calculate(y: Long): Long = x + y
     }
     class sub(x: Long) : Operator(x) {
-        override fun calculate(y: Long): Long {
-            return x - y
-        }
+        override fun calculate(y: Long): Long = x - y
     }
     class mult(x: Long) : Operator(x) {
         override fun calculate(y: Long): Long = x * y
